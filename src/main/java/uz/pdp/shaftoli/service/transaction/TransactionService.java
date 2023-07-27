5d23ec33b713278568ab86159fa3e6d106e72344
Bekzod <bekzodhamidullayev16@gmail.com>
2023-07-28 01:30:35 +0500

    Nodirbek qabul qilvoring

---
+++ src/main/java/uz/pdp/shaftoli/service/transaction/TransactionService.java
@@ -0,0 +1,7 @@
+package uz.pdp.shaftoli.service.transaction;
+
+import uz.pdp.shaftoli.model.Transaction;
+import uz.pdp.shaftoli.service.BaseService;
+
+public interface TransactionService extends BaseService<Transaction> {
+}
---
+++ src/main/java/uz/pdp/shaftoli/service/transaction/TransactionServiceImpl.java
@@ -0,0 +1,22 @@
+package uz.pdp.shaftoli.service.transaction;
+
+import uz.pdp.shaftoli.model.Transaction;
+
+import java.util.ArrayList;
+
+public class TransactionServiceImpl implements TransactionService{
+    @Override
+    public Transaction add(Transaction transaction) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Transaction> getAll() {
+        return null;
+    }
+
+    @Override
+    public Transaction getById() {
+        return null;
+    }
+}
