84ec145b31843a485edea307211401c549fd2173
Ali <thealiergashev@gmail.com>
2023-07-27 10:08:22 +0500

    lumbok

--- src/main/java/uz/pdp/shaftoli/model/Card.java
+++ src/main/java/uz/pdp/shaftoli/model/Card.java
@@ -1,7 +1,16 @@
 package uz.pdp.shaftoli.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Card extends BaseModel{
     private UUID ownerId;
     private String cardNumber;
