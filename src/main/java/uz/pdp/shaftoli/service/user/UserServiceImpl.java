2a99e4cb32f8f7179e4ae0454c4b6ffbf649fd93
Bekzod <bekzodhamidullayev16@gmail.com>
2023-07-27 11:51:38 +0500

    add(service)

--- src/main/java/uz/pdp/shaftoli/service/user/UserServiceImpl.java
+++ src/main/java/uz/pdp/shaftoli/service/user/UserServiceImpl.java
@@ -3,15 +3,17 @@ package uz.pdp.shaftoli.service.user;
 import uz.pdp.shaftoli.model.User;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class UserServiceImpl implements UserService{
     @Override
     public User add(User user) {
-        return null;
+        return user;
     }
 
     @Override
     public ArrayList<User> getAll() {
+
         return null;
     }
 
