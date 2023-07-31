f5b49009877ca8398cb87091275f1f6a4c60b62f
Bekzod <bekzodhamidullayev16@gmail.com>
2023-07-31 17:54:14 +0500

    kerakmas

--- src/main/java/uz/pdp/shaftoli/service/user/UserService.java
+++ src/main/java/uz/pdp/shaftoli/service/user/UserService.java
@@ -4,7 +4,8 @@ import uz.pdp.shaftoli.entity.UserEntity;
 import uz.pdp.shaftoli.service.BaseService;
 
 
-public interface UserService extends BaseService<UserEntity> {
+public interface
+UserService extends BaseService<UserEntity> {
     void userValidation(String email);
 
 }
