b553ae524d70de4f426e1549dd8ec9384a70c546
nodirbekomonboyev <omonboevnodir77@gmail.com>
2023-08-01 10:38:55 +0500

    change

--- src/main/java/uz/pdp/shaftoli/repository/email/EmailCodeRepository.java
+++ src/main/java/uz/pdp/shaftoli/repository/email/EmailCodeRepository.java
@@ -1,7 +1,7 @@
 package uz.pdp.shaftoli.repository.email;
 
 public interface EmailCodeRepository {
-    String UPDATE_EMAIL_CODE = "update email_code set email = ?, code = ?, limit = ? where email = :email";
+    String UPDATE_EMAIL_CODE = "update email_code set email = :email, code = :code, limit = :limit";
     String GET_BY_EMAIL_CODE = "select ec from email_code ec where email = :email";
     String CREATE_EMAIL_CODE = "insert into email_code(email, code, limit) values(:email,:code,:limit)";
     void saveEmail(String email, String emailCode);
