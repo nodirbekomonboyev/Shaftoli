45a144232fcac1a5a0f10c7ad6ba35d5d9b6ff33
Erpolat <erpolatjanabergenov25@gmail.com>
2023-08-02 18:59:38 +0500

    Transaction Service

--- src/main/java/uz/pdp/shaftoli/service/transaction/TransactionServiceImpl.java
+++ src/main/java/uz/pdp/shaftoli/service/transaction/TransactionServiceImpl.java
@@ -1,20 +1,28 @@
 package uz.pdp.shaftoli.service.transaction;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.pdp.shaftoli.entity.TransactionEntity;
+import uz.pdp.shaftoli.repository.transaction.TransactionRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService{
+
+    private final TransactionRepository transactionRepository;
+
     @Override
     public TransactionEntity add(TransactionEntity transaction) {
-        return null;
+        return transactionRepository.save(transaction);
     }
 
     @Override
-    public ArrayList<TransactionEntity> getAll() {
-        return null;
+    public  List<TransactionEntity> getAll() {
+        return  transactionRepository.getAll();
+
     }
 
     @Override
